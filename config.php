147eaa35b278b19d9e8b3a4254087ed2d268a0cf
PHP科科 <liutongke@live.com>
2018-07-26 16:49:54 +0800

    修改为依赖注入，将openid、message、close分开使用，引入第三方的DB类

--- config.php
+++ config.php
@@ -2,7 +2,7 @@
 /*
  * User: keke
  * Date: 2018/7/26
- * Time: 14:10
+ * Time: 16:19
  *——————————————————佛祖保佑 ——————————————————
  *                   _ooOoo_
  *                  o8888888o
@@ -24,33 +24,11 @@
  *                   `=---='
  *——————————————————代码永无BUG —————————————————
  */
-require_once __DIR__ . '/vendor/autoload.php';
-require_once __DIR__ . '/function.php';
-use \swoole\Websocket;
-
-$webSocket = new Websocket();
-$webSocket->run();
-
-////创建websocket服务器对象，监听0.0.0.0:9502端口
-//$ws = new swoole_websocket_server("0.0.0.0", 9502);
-//
-////监听WebSocket连接打开事件
-//$ws->on('open', function ($ws, $request) {
-//    //生成依赖
-//    $msgMethod = new Open();
-//    //注入依赖
-//    $pb = new SendMsg($msgMethod);
-//    $pb->send($ws, $request);
-//});
-//
-////监听WebSocket消息事件
-//$ws->on('message', function ($ws, $frame) {
-////    $ws->push($frame->fd, "server: {$frame->data}");
-//});
-//
-////监听WebSocket连接关闭事件
-//$ws->on('close', function ($ws, $fd) {
-////    echo "client-{$fd} is closed\n";
-//});
-//
-//$ws->start();
+//配置
+return [
+    'host' => '121.196.192.76',
+    'port' => 3306,
+    'name' => 'test',
+    'user' => 'root',
+    'pass' => '13516421896',
+];
--- src/SendMsg.php
+++ src/SendMsg.php
@@ -1,8 +1,9 @@
 <?php
+
 /*
  * User: keke
  * Date: 2018/7/26
- * Time: 14:10
+ * Time: 14:42
  *——————————————————佛祖保佑 ——————————————————
  *                   _ooOoo_
  *                  o8888888o
@@ -24,33 +25,21 @@
  *                   `=---='
  *——————————————————代码永无BUG —————————————————
  */
-require_once __DIR__ . '/vendor/autoload.php';
-require_once __DIR__ . '/function.php';
-use \swoole\Websocket;
 
-$webSocket = new Websocket();
-$webSocket->run();
+namespace swoole;
 
-////创建websocket服务器对象，监听0.0.0.0:9502端口
-//$ws = new swoole_websocket_server("0.0.0.0", 9502);
-//
-////监听WebSocket连接打开事件
-//$ws->on('open', function ($ws, $request) {
-//    //生成依赖
-//    $msgMethod = new Open();
-//    //注入依赖
-//    $pb = new SendMsg($msgMethod);
-//    $pb->send($ws, $request);
-//});
-//
-////监听WebSocket消息事件
-//$ws->on('message', function ($ws, $frame) {
-////    $ws->push($frame->fd, "server: {$frame->data}");
-//});
-//
-////监听WebSocket连接关闭事件
-//$ws->on('close', function ($ws, $fd) {
-////    echo "client-{$fd} is closed\n";
-//});
-//
-//$ws->start();
+class SendMsg
+{
+
+    private $sw;
+
+    public function __construct(Chat $payMethod)
+    {
+        $this->sw = $payMethod;
+    }
+
+    public function send($ws, $request)
+    {
+        $this->sw->Handle($ws, $request);
+    }
+}
