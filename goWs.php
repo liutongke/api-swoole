036271eba5dac8223c65b4becf9934712456abd3
suerface <1338555+tongke@user.noreply.gitee.com>
2021-07-11 13:42:42 +0800

    demo

---
+++ goWs.php
@@ -0,0 +1,74 @@
+<?php
+/*
+ * User: keke
+ * Date: 2021/7/11
+ * Time: 2:20
+ *——————————————————佛祖保佑 ——————————————————
+ *                   _ooOoo_
+ *                  o8888888o
+ *                  88" . "88
+ *                  (| -_- |)
+ *                  O\  =  /O
+ *               ____/`---'\____
+ *             .'  \|     |//  `.
+ *            /  \|||  :  |||//  \
+ *           /  _||||| -:- |||||-  \
+ *           |   | \\  -  /// |   |
+ *           | \_|  ''\---/''  |   |
+ *           \  .-\__  `-`  ___/-. /
+ *         ___`. .'  /--.--\  `. . __
+ *      ."" '<  `.___\_<|>_/___.'  >'"".
+ *     | | :  ` - `.;`\ _ /`;.`/ - ` : | |
+ *     \  \ `-.   \_ __\ /__ _/   .-` /  /
+ *======`-.____`-.___\_____/___.-`____.-'======
+ *                   `=---='
+ *——————————————————代码永无BUG —————————————————
+ */
+
+use Swoole\Process;
+use Swoole\Coroutine;
+use Swoole\Coroutine\Server\Connection;
+
+
+use Swoole\Http\Request;
+use Swoole\Http\Response;
+use Swoole\WebSocket\CloseFrame;
+use Swoole\Coroutine\Http\Server;
+use function Swoole\Coroutine\run;
+
+//多进程管理模块
+$pool = new Process\Pool(2);
+//让每个OnWorkerStart回调都自动创建一个协程
+$pool->set(['enable_coroutine' => true]);
+$pool->on('workerStart', function ($pool, $id) {
+    $server = new Swoole\Coroutine\Http\Server('0.0.0.0', 9500, false, true);
+    $server->handle('/websocket', function (Request $request, Response $ws) {
+        $ws->upgrade();
+        global $wsObjects;
+        $objectId = spl_object_id($ws);
+        $wsObjects[$objectId] = $ws;
+        while (true) {
+            $frame = $ws->recv();
+            if ($frame === '') {
+                unset($wsObjects[$objectId]);
+                $ws->close();
+                break;
+            } else if ($frame === false) {
+                echo 'errorCode: ' . swoole_last_error() . "\n";
+                $ws->close();
+                break;
+            } else {
+                if ($frame->data == 'close' || get_class($frame) === CloseFrame::class) {
+                    unset($wsObjects[$objectId]);
+                    $ws->close();
+                    break;
+                }
+                foreach ($wsObjects as $obj) {
+                    $obj->push("Server：{$frame->data}");
+                }
+            }
+        }
+    });
+    $server->start();
+});
+$pool->start();
