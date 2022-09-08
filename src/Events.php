fd8d23b4d98dee2a9ea95f931a23b0c71177386a
keke <1338555+tongke@user.noreply.gitee.com>
2022-09-08 16:53:37 +0800

    增加用户自定义的工作进程工程

--- src/Events.php
+++ src/Events.php
@@ -38,12 +38,16 @@ class Events
 
     public function onOpen(\Swoole\WebSocket\Server $server, $request)
     {
-        echo "server: handshake success with fd{$request->fd}\n";
+        if (DI()->config->get('conf.debug')) {
+            echo "server: handshake success with fd{$request->fd}\n";
+        }
     }
 
     public function onClose($ser, $fd)
     {
-        echo "client {$fd} closed\n";
+        if (DI()->config->get('conf.debug')) {
+            echo "client {$fd} closed\n";
+        }
     }
 
     public function onMessage(\Swoole\WebSocket\Server $server, $frame)
@@ -74,9 +78,7 @@ class Events
     //使用 task 必须为 Server 设置 onTask 和 onFinish 回调，否则 Server->start 会失败
     public function onTask(\Swoole\Server $server, int $task_id, int $src_worker_id, array $data)
     {
-        echo "Tasker进程接收到数据,task_id:";
-        var_dump($task_id);
-        var_dump($data);
+        echo "Tasker进程接收到数据,task_id:{$task_id}";
     }
 
     public function onFinish(\Swoole\Server $server, int $task_id, int $src_worker_id, array $data)
