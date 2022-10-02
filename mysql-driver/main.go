7eaba70a15efc2dddb7c6bb478d02df510ff19ec
keke <1338555+tongke@user.noreply.gitee.com>
2022-10-02 16:01:27 +0800

    MySQL binlog、伪装slave从服务器获取binlog

--- mysql-driver/main.go
+++ mysql-driver/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"go-mysql/binlog/binlog"
 	"go-mysql/binlog/packet"
 	"go-mysql/binlog/server"
 )
@@ -8,14 +9,23 @@ import (
 //var sequenceId uint8 = 1 //包序列id
 
 func main() {
+	//binlog.Binlog()
+	//return
 	mysql := server.NewMysql("root", "root", "192.168.0.107", "3306")
 	//mysql := server.NewMysql("root", "REMOVED", "192.168.0.105", "3304")
 	//go PingTimer(Ping, mysql, 10*time.Second)
 
 	authPacket := packet.NewHandshake().ReadAuthResult(mysql)
 	mysql.Write(authPacket, 1) //发送auth Packet
+	mysql.ShowMaster()
+	mysql.SetChecksum()
+	mysql.SetSlaveUuid()
+	mysql.Show()
+	mysql.RegisterSlave()
+	mysql.Write(binlog.Binlog(), 0)
 	for {
 		packetData := mysql.Payload()
+		//fmt.Println(packetData)
 		packet.NewPacket().Handler(packetData, mysql)
 		typeSql := server.UserInput()
 
--- mysql-driver/server/order.go
+++ mysql-driver/server/order.go
@@ -1,6 +1,9 @@
 package server
 
-import "strings"
+import (
+	"encoding/hex"
+	"strings"
+)
 
 func GetOrder(idx string) uint8 {
 	m := map[string]uint8{
@@ -17,6 +20,39 @@ func GetOrder(idx string) uint8 {
 	}
 
 	return m[strings.ToLower(idx)]
+}
+
+// SET @master_heartbeat_period= 30000001024
+// SET @master_binlog_checksum= @@global.binlog_checksum
+// SET @slave_uuid= '62c16f3c-41ab-11ed-8d8e-0242ac110002'
+//163	5.714582	192.168.0.105	192.168.0.107	MySQL	76	Request Register Slave
+
+func (conn *Mysql) ShowMaster() {
+	var bytes = []byte{0x03}
+	conn.Write(append(bytes, []byte("show master status;")...), 0)
+}
+func (conn *Mysql) Show() {
+	var bytes = []byte{0x03}
+	conn.Write(append(bytes, []byte("show global variables like 'binlog_checksum';")...), 0)
+}
+
+func (conn *Mysql) SetSlaveUuid() {
+	var bytes = []byte{0x03}
+	conn.Write(append(bytes, []byte("SET @slave_uuid= '6ac0fdae-b5d7-11e4-a9f3-0800278ce5c9';")...), 0)
+}
+
+func (conn *Mysql) SetChecksum() {
+	var bytes = []byte{0x03}
+	conn.Write(append(bytes, []byte("set @master_binlog_checksum= @@global.binlog_checksum;")...), 0)
+}
+
+func (conn *Mysql) RegisterSlave() {
+	var bytes = []byte{0x15}
+	decodeString, err := hex.DecodeString("02000000000000ea0c0000000000000000")
+	if err != nil {
+		return
+	}
+	conn.Write(append(bytes, decodeString...), 0)
 }
 
 func (conn *Mysql) SetChart() []byte {
