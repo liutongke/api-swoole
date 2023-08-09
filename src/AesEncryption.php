0987db3ab1fa511f996d646ff7e24fe69510d5da
keke <liutongke@live.com>
2023-08-10 01:21:56 +0800

    用静态变量代替了密钥长度选项。 在方法参数和返回值上添加了更明确的类型提示。 使用 string 替代了 mixed，以增强代码类型安全性。 对注释进行了微小调整，以使其更具一致性和可读性。

--- src/AesEncryption.php
+++ src/AesEncryption.php
@@ -4,17 +4,38 @@ namespace Sapi;
 
 class AesEncryption
 {
-    private $key;
+    private string $key;
 
-    public function __construct($key)
+    // AES 密钥长度选项
+    public static int $KEY_LEN_EASY = 16;
+    public static int $KEY_LEN_MEDIUM = 24;
+    public static int $KEY_LEN_DIFFICULTY = 32;
+
+    public function __construct(int $keyLen)
     {
-        if (empty($key) || strlen($key) != 16) {
-//            throw new Exception("AES key must be a 16-character string");
-        }
-        $this->key = $key;
+        $this->key = $this->generateKey($keyLen);
     }
 
-    public function encrypt($data)
+    /**
+     * 生成 AES 密钥
+     *
+     * @param int $keyLen 密钥长度
+     * @return string 生成的密钥
+     */
+    private function generateKey(int $keyLen): string
+    {
+        $aesKey = openssl_random_pseudo_bytes($keyLen);
+        return bin2hex($aesKey);
+    }
+
+    /**
+     * 对数据进行 AES 加密
+     *
+     * @param string $data 待加密的数据
+     * @return string 加密后的数据
+     * @throws \Exception
+     */
+    public function encrypt(string $data): string
     {
         $iv = random_bytes(16); // 随机生成初始化向量
         $ciphertext = openssl_encrypt($data, 'aes-128-cbc', $this->key, OPENSSL_RAW_DATA, $iv);
@@ -22,7 +43,13 @@ class AesEncryption
         return base64_encode($encryptedData);
     }
 
-    public function decrypt($encryptedData)
+    /**
+     * 对 AES 加密的数据进行解密
+     *
+     * @param string $encryptedData 加密的数据
+     * @return string 解密后的数据
+     */
+    public function decrypt(string $encryptedData): string
     {
         $encryptedData = base64_decode($encryptedData);
         $iv = substr($encryptedData, 0, 16);
